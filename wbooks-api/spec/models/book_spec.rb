bd379add75819d5a7bf32c916d1fcfddbc709bb8
edkajimenez <42581828+edkajimenez@users.noreply.github.com>
2018-09-03 15:31:37 -0300

    Books test and config (#9)
    * Add rspec, factory_girl, faker and database_cleaner gems. Book test
    
    * user example model test, book model test, user and book factories
    
    * style fixes

---
+++ wbooks-api/spec/models/book_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe Book do
+  it { should validate_presence_of(:genre) }
+  it { should validate_presence_of(:author) }
+  it { should validate_presence_of(:image) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:publisher) }
+  it { should validate_presence_of(:year) }
+
+  it do
+    expect(build(:book)).to be_valid
+  end
+
+  describe '#create' do
+    context 'When the genre is nil' do
+      it 'is not valid' do
+        expect(build(:book, genre: nil)).not_to be_valid
+      end
+    end
+
+    context 'When the author is nil' do
+      it 'is not valid' do
+        expect(build(:book, author: nil)).not_to be_valid
+      end
+    end
+
+    context 'When the image is nil' do
+      it 'is not valid' do
+        expect(build(:book, image: nil)).not_to be_valid
+      end
+    end
+
+    context 'When the title is nil' do
+      it 'is not valid' do
+        expect(build(:book, title: nil)).not_to be_valid
+      end
+    end
+
+    context 'When the publisher is nil' do
+      it 'is not valid' do
+        expect(build(:book, publisher: nil)).not_to be_valid
+      end
+    end
+
+    context 'When the year is nil' do
+      it 'is not valid' do
+        expect(build(:book, year: nil)).not_to be_valid
+      end
+    end
+  end
+end
