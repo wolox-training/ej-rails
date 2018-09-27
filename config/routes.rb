57c1fe355446ace4b8a41f5292df49d77730222b
Edkaryd Jimenez <edkaryd.jimenez@wolox.com.ar>
2018-09-27 12:28:22 -0300

    book suggestion route

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,14 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get 'books/find_by/:isbn', to: 'books#find_by_isbn'
+  get '/books/find_by/:isbn', to: 'books#find_by_isbn'
+  get '/book_suggestions', to: 'book_suggestions#create'
+
   resources :books, only: %i[index show] do
     resources :rents, only: %i[index]
   end
-
+  
   resources :users, only: %i[index show create] do
     resources :rents, only: %i[index create]
   end
-
 end
