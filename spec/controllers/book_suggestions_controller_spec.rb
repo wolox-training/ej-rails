feac32e597e054ea1a446f4a1d43917755d8e4c0
Edkaryd Jimenez <edkaryd.jimenez@wolox.com.ar>
2018-09-27 12:56:50 -0300

    book suggestion test

--- spec/controllers/book_suggestions_controller_spec.rb
+++ spec/controllers/book_suggestions_controller_spec.rb
@@ -3,6 +3,64 @@
 require 'rails_helper'
 
 RSpec.describe BookSuggestionsController, type: :controller do
+  include_context 'Authenticated User'
+
   describe 'GET #create' do
+    context 'When logged user' do
+      let(:book_suggestion) { attributes_for(:book_suggestion, user_id: user.id) }
+      before do
+        post :create, params: { book_suggestion: book_suggestion }
+      end
+
+      it 'returns http status created' do
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'When guest user' do
+      let(:book_suggestion) { attributes_for(:book_suggestion, user_id: nil) }
+      before do
+        post :create, params: { book_suggestion: book_suggestion }
+      end
+
+      it 'returns http status created' do
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'When there is a missing param' do
+      context 'When the title is missing' do
+        let(:book_suggestion) { attributes_for(:book_suggestion, title: nil) }
+        before do
+          post :create, params: { book_suggestion: book_suggestion }
+        end
+
+        it 'returns http status 400' do
+          expect(response).to have_http_status(:bad_request)
+        end
+      end
+
+      context 'When the author is missing' do
+        let(:book_suggestion) { attributes_for(:book_suggestion, author: nil) }
+        before do
+          post :create, params: { book_suggestion: book_suggestion }
+        end
+
+        it 'returns http status 400' do
+          expect(response).to have_http_status(:bad_request)
+        end
+      end
+
+      context 'When the link is missing' do
+        let(:book_suggestion) { attributes_for(:rent, link: nil) }
+        before do
+          post :create, params: { book_suggestion: book_suggestion }
+        end
+
+        it 'returns http status 400' do
+          expect(response).to have_http_status(:bad_request)
+        end
+      end
+    end
   end
 end
